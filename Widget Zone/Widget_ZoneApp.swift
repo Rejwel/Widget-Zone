04b19aff365cec21fd04fc8b7ce12987c248dcdd
Pawel Dera <paweldera01@gmail.com>
2021-10-02 13:32:10 +0200

    Initial commit

---
+++ Widget Zone/Widget_ZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widget_ZoneApp.swift
+//  Widget Zone
+//
+//  Created by rey on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Widget_ZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
